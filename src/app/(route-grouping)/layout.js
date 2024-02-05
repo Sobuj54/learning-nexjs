5105db5a354b1399f68233e66ed3d5d5d1041485
soubj <sobujahmed124@gmail.com>
2024-02-05 21:49:26 +0600

    not found page and route grouping

---
+++ src/app/(route-grouping)/layout.js
@@ -0,0 +1,10 @@
+const layout = ({ children }) => {
+  return (
+    <div>
+      <nav>route grouping</nav>
+      {children}
+    </div>
+  );
+};
+
+export default layout;
---
+++ src/app/not-found.js
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-5xl text-center font-bold text-red-500">404</h1>
+    </div>
+  );
+};
+
+export default NotFound;
